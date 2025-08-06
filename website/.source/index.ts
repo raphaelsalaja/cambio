28f31e9ad30b690aff569fa4f158323d107f8c68
Raphael S. <52125687+raphaelsalaja@users.noreply.github.com>
2025-08-06 21:55:06 +0100

    refactor: improve code formatting and organization in source files

--- website/.source/index.ts
+++ website/.source/index.ts
@@ -1,5 +1,18 @@
 // @ts-nocheck -- skip type checking
-import * as docs_0 from "../content/documentation.mdx?collection=docs&hash=1754402745468"
-import { _runtime } from "fumadocs-mdx"
-import * as _source from "../source.config"
-export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"documentation.mdx","absolutePath":"/Users/raphaelsalaja/Developer/cambio/website/content/documentation.mdx"}, data: docs_0 }], [])
+
+import { _runtime } from "fumadocs-mdx";
+import * as docs_0 from "../content/documentation.mdx?collection=docs&hash=1754402745468";
+import type * as _source from "../source.config";
+export const docs = _runtime.docs<typeof _source.docs>(
+  [
+    {
+      info: {
+        path: "documentation.mdx",
+        absolutePath:
+          "/Users/raphaelsalaja/Developer/cambio/website/content/documentation.mdx",
+      },
+      data: docs_0,
+    },
+  ],
+  [],
+);
--- website/mdx-components.tsx
+++ website/mdx-components.tsx
@@ -2,7 +2,7 @@ import type { MDXComponents } from "mdx/types";
 import { Examples } from "@/components/examples";
 
 export function getMDXComponents(): MDXComponents {
-	return {
-		...Examples,
-	};
+  return {
+    ...Examples,
+  };
 }
